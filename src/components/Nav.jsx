ee8434f3d300193863fb557a7b3fe633037307ef
Gustavo Souza <gustavoelia7@gmail.com>
2025-04-28 16:58:23 -0300

    more text animations, background change

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,12 +3,22 @@ import { NavLink } from 'react-router';
 
 export default function Nav() {
   return (
-    <div className="header">
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-      </nav>
-    </div>
+    <>
+      <div className="header">
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </nav>
+      </div>
+      <div className="menu-toggle">
+        <div className="icon">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div className="menu"></div>
+    </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/all';
+import { CustomEase, ScrollTrigger } from 'gsap/all';
 
 export default function Home() {
   // const subtitle =
@@ -10,20 +10,134 @@ export default function Home() {
       ' '
     );
 
-  const paragraph =
+  const paragraph1 =
     'Combining expertise in UX/UI design and front-end development, I craft adaptable websites that seamlessly integrate motion, interaction, and design to deliver compelling user experiences. My work prioritizes simplicity and clarity, ensuring every detail is thoughtfully designed and precisely implemented.'.split(
+      ' '
+    );
+
+  const paragraph21 =
+    'Our approach at OH Architecture is designed to make your journey from concept to completion as smooth and enjoyable as possible.'.split(
+      ' '
+    );
+  const paragraph22 =
+    'With our 6-stages plan, we prioritize clarity, collaboration, and your unique vision. Each stage is crafted to keep you informed, inspired, and involved.'.split(
       ' '
     );
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
-    const sections = gsap.utils.toArray('.section');
+    // change page background
+
+    const darkSections = document.querySelectorAll('.section.dark');
+    const whiteSections = document.querySelectorAll('.section.white');
+
+    darkSections.forEach((section) => {
+      ScrollTrigger.create({
+        trigger: section,
+        start: 'top 35%',
+        end: 'bottom 70%',
+
+        onEnter: () => {
+          gsap.to('.content', {
+            backgroundColor: '#0a0a0a',
+            color: '#fff',
+          });
+
+          gsap.to('.menu-toggle', {
+            backgroundColor: '#fff',
+          });
+
+          gsap.to('.menu-toggle span', {
+            backgroundColor: '#0a0a0a',
+          });
+        },
+
+        onLeaveBack: () => {
+          gsap.to('.content', {
+            backgroundColor: '#fff',
+            color: '#0a0a0a',
+          });
+
+          gsap.to('.menu-toggle', {
+            backgroundColor: '#0a0a0a',
+          });
+
+          gsap.to('.menu-toggle span', {
+            backgroundColor: '#fff',
+          });
+        },
+      });
+    });
+
+    whiteSections.forEach((section) => {
+      ScrollTrigger.create({
+        trigger: section,
+        start: 'top 35%',
+        end: 'bottom 70%',
+
+        onEnter: () => {
+          gsap.to('.content', {
+            backgroundColor: '#fff',
+            color: '#0a0a0a',
+          });
+
+          gsap.to('.menu-toggle', {
+            backgroundColor: '#0a0a0a',
+          });
+
+          gsap.to('.menu-toggle span', {
+            backgroundColor: '#fff',
+          });
+        },
+
+        onLeaveBack: () => {
+          gsap.to('.content', {
+            backgroundColor: '#0a0a0a',
+            color: '#fff',
+          });
+
+          gsap.to('.menu-toggle', {
+            backgroundColor: '#fff',
+          });
+
+          gsap.to('.menu-toggle span', {
+            backgroundColor: '#0a0a0a',
+          });
+        },
+      });
+    });
+
+    // menu toggle button
+
+    ScrollTrigger.create({
+      trigger: '.section2',
+      start: 'top 70%',
+      end: 'bottom 70%',
+
+      onEnter: () => {
+        gsap.to('.menu-toggle', {
+          x: 0,
+          ease: 'expo.out',
+          duration: 0.5,
+        });
+      },
+
+      onLeaveBack: () => {
+        gsap.to('.menu-toggle', {
+          x: 100,
+          ease: 'expo.out',
+          duration: 0.5,
+        });
+      },
+    });
+
+    // first section
 
     const tl1 = gsap.timeline({
       defaults: {
         ease: 'expo.out',
-        duration: 0.5,
+        duration: 1,
       },
     });
 
@@ -43,17 +157,57 @@ export default function Home() {
         '.subtitle .line .text',
         {
           y: 0,
-          stagger: 0.01,
+          stagger: 0.008,
           rotate: '0',
           ease: 'expo.out',
         },
         '<0.3'
       );
 
+    // second section
+
+    const tl2 = gsap.timeline({
+      defaults: {
+        duration: 0.5,
+      },
+      scrollTrigger: {
+        trigger: '.section2',
+        start: 'top 35%',
+        end: 'bottom 70%',
+        toggleActions: 'play none none reverse',
+      },
+    });
+
+    tl2.to('.section2 .paragraph .line .text', {
+      y: 0,
+      stagger: 0.015,
+      rotate: '0',
+      // ease: 'power2.out'
+      // ease: CustomEase.create("custom", "M0,0 C0.126,0.382 0.146,0.676 0.304,0.824 0.496,1.004 0.818,1.001 1,1 "),
+      ease: CustomEase.create('custom', 'M0,0 C0.126,0.382 0.063,0.623 0.221,0.771 0.413,0.951 0.818,1.001 1,1 '),
+    });
+
+    // other sections
+
+    const sections = gsap.utils.toArray('.section');
+
     sections.forEach((section, index) => {
       const paragraph = section.querySelectorAll('.paragraph .line .text');
 
-      if (index !== 0) {
+      const sectionsTl = gsap.timeline({
+        defaults: {
+          ease: 'expo.out',
+        },
+        scrollTrigger: {
+          trigger: section,
+          start: 'top 35%',
+          end: 'bottom 70%',
+          // toggleActions: 'play reverse play reverse',
+          toggleActions: 'play none none reverse',
+        },
+      });
+
+      if (index !== 0 && index !== 1) {
         // ScrollTrigger.create({
         //   trigger: section,
         //   start: 'top top',
@@ -62,21 +216,7 @@ export default function Home() {
         // 	start: 'top 100px'
         // });
 
-        const tl2 = gsap.timeline({
-          defaults: {
-            ease: 'expo.out',
-          },
-          scrollTrigger: {
-            trigger: section,
-            start: 'top 35%',
-            end: 'bottom 70%',
-            // toggleActions: 'play reverse play reverse',
-            toggleActions: 'play none none reverse',
-            // markers: true,
-          },
-        });
-
-        tl2.to(
+        sectionsTl.to(
           paragraph,
           {
             y: 0,
@@ -91,7 +231,7 @@ export default function Home() {
     });
 
     return () => {
-			// cleanup when component unmounts
+      // cleanup when component unmounts
       ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     };
   }, []);
@@ -108,7 +248,7 @@ export default function Home() {
         </p>
       </div> */}
 
-      <div className="section section1">
+      <div className="section section1 dark">
         <h1 className="box title">
           <span className="line">
             <span className="text">Gustavo</span>
@@ -128,9 +268,16 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="section section2">
+      <div className="section section2 white">
         <div className="box paragraph">
-          {paragraph.map((word, index) => (
+          {paragraph21.map((word, index) => (
+            <div className="line" key={index}>
+              <div className="text">{word}&nbsp;</div>
+            </div>
+          ))}
+        </div>
+        <div className="box paragraph">
+          {paragraph22.map((word, index) => (
             <div className="line" key={index}>
               <div className="text">{word}&nbsp;</div>
             </div>
@@ -138,9 +285,9 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="section section3">
+      <div className="section section3 dark">
         <div className="box paragraph">
-          {paragraph.map((word, index) => (
+          {paragraph1.map((word, index) => (
             <div className="line" key={index}>
               <div className="text">{word}&nbsp;</div>
             </div>
@@ -148,9 +295,9 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="section section4">
+      <div className="section section4 white">
         <div className="box paragraph">
-          {paragraph.map((word, index) => (
+          {paragraph1.map((word, index) => (
             <div className="line" key={index}>
               <div className="text">{word}&nbsp;</div>
             </div>
@@ -158,9 +305,9 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="section section5">
+      <div className="section section5 dark">
         <div className="box paragraph">
-          {paragraph.map((word, index) => (
+          {paragraph1.map((word, index) => (
             <div className="line" key={index}>
               <div className="text">{word}&nbsp;</div>
             </div>
