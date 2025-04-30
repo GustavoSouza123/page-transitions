6613f565b58c5d7dc196ec5371c102de80609b1f
Gustavo Souza <gustavoelia7@gmail.com>
2025-04-30 18:38:36 -0300

    menu links hover animation easing

--- src/components/Navigation/NavItem.jsx
+++ src/components/Navigation/NavItem.jsx
@@ -3,20 +3,26 @@ import { NavLink } from 'react-router';
 import { motion } from 'motion/react';
 
 export default function NavItem({ children, href }) {
-  const MotionNavLink = motion(NavLink);
+  const MotionNavLink = motion.create(NavLink);
 
   /* to do: delay navigation to close menu and scroll up */
   const handleMenuClick = () => {
     toggleOpen();
   };
 
+  const transition = {
+    duration: 0.5,
+    ease: [0, 0.96, 0.4, 1],
+  };
+
   return (
     <div className="item">
-      <MotionNavLink to={href} onClick={handleMenuClick} initial="initial" whileHover="hover">
+      <MotionNavLink to={href} onClick={handleMenuClick} initial="initial" whileHover="hover" animate="animate">
         <motion.div
           variants={{
             initial: { y: 0 },
-            hover: { y: '-100%' },
+            hover: { y: '-100%', transition },
+            animate: { y: 0, transition },
           }}
         >
           {children}
@@ -25,7 +31,8 @@ export default function NavItem({ children, href }) {
           style={{ position: 'absolute', top: '0' }}
           variants={{
             initial: { y: '100%' },
-            hover: { y: 0 },
+            hover: { y: 0, transition },
+            animate: { y: '100%', transition },
           }}
         >
           {children}
